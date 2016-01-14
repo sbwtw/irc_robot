032771cb787b5063fe3ea2a1fd8f3c6b5a7cf593
石博文 <sbw@sbw.so>
2016-01-14 20:59:48 +0800

    clean some code

--- src/irc_client/irc_client.rs
+++ src/irc_client/irc_client.rs
@@ -64,42 +64,7 @@ impl IRCClient {
     }
 
     pub fn startup(&mut self) {
-        //println!("Startup");
-
-        //self.connect();
-
-        //// waitting for server response
-        //let (socket_tx, socket_rx) = channel();
-        //let mut reader = match self.irc_socket.as_mut() {
-            //Some(x) => BufReader::new(x.try_clone().unwrap()),
-            //None => panic!("Socket Error"),
-        //};
-        //let receive_thread = thread::spawn(move || {
-            //loop {
-                //let mut buf_line: String = String::new();
-                //let _ = reader.read_line(&mut buf_line);
-                //if buf_line.len() == 0 {
-                    //continue;
-                //}
-
-                //let _ = socket_tx.send(buf_line).unwrap();
-            //}
-        //});
-
-        //// waitting for user command
-        //let (command_tx, command_rx) = channel();
-        //let command_thread = thread::spawn(move || {
-            //let stdin = stdin();
-            //loop {
-                //let mut command: String = String::new();
-                //let mut handle = stdin.lock();
-                //let _ = handle.read_line(&mut command);
-
-                //let _ = command_tx.send(command);
-            //}
-        //});
-
-        //// login
+        // login
         let command = &format!("PASS {}", &self.irc_password);
         self.command(command);
         let command = &format!("NICK {}", &self.irc_nick_name);
@@ -109,33 +74,6 @@ impl IRCClient {
         self.command(command);
 
         self.join_channels();
-
-        //'msg: loop {
-            //// check command
-            //let command = command_rx.try_recv();
-            //if command.is_ok() {
-                //let command: String = command.unwrap();
-
-                //// quit
-                //if command == "exit\n" ||
-                   //command == "EXIT\n" {
-                    //break 'msg;
-                //}
-
-                //self.command(&command);
-            //}
-
-            //// check socket
-            //let response = socket_rx.try_recv();
-            //if response.is_ok() {
-                //let response = response.unwrap();
-                //self.process(&response);
-            //}
-        //}
-
-        //println!("Waitting for thread finished.");
-        //let _ = receive_thread.join();
-        //let _ = command_thread.join();
     }
 
     // execute a command
@@ -230,10 +168,12 @@ impl IRCClient {
         let mut buf: String = String::new();
         self.irc_socket.read_to_string(&mut buf);
 
-        println!("read: {}", buf);
+        let list: Vec<&str> = buf.split('\n').collect();
 
-        if let Ok(msg) = buf.parse::<Message>() {
-            self.process(&msg);
+        for item in list {
+            if let Ok(msg) = item.parse::<Message>() {
+                self.process(&msg);
+            }
         }
     }
 
